8c7017f255cf315728ad8708ba17ac38b613e8fd
numso <dosmun@gmail.com>
2014-10-07 16:41:31 -0600

    added a command center

--- leap-server/server/sockets/swype.js
+++ leap-server/server/sockets/swype.js
@@ -13,21 +13,19 @@ module.exports = function (io) {
   var tolerance = 50;
   var cooloff = 300;
 
-  var x = 2, y = 2;
-
-  var slider = _.debounce(function(xDir, yDir) {
+  var slider = _.debounce(function(xDir, zDir) {
     console.log('x: ' + xDir);
-    console.log('y: ' + yDir);
+    console.log('z: ' + zDir);
     console.log();
     if (xDir) io.emit('leap:swype-lr', xDir);
-    if (yDir) io.emit('leap:swipe-tb', yDir);
+    if (zDir) io.emit('leap:swipe-tb', zDir);
   }, cooloff);
 
   swiper.update(function(g) {
-    if (Math.abs(g.translation()[0]) > tolerance || Math.abs(g.translation()[1]) > tolerance) {
+    if (Math.abs(g.translation()[0]) > tolerance || Math.abs(g.translation()[2]) > tolerance) {
       var xDir = Math.abs(g.translation()[0]) > tolerance ? (g.translation()[0] > 0 ? -1 : 1) : 0;
-      var yDir = Math.abs(g.translation()[1]) > tolerance ? (g.translation()[1] < 0 ? -1 : 1) : 0;
-      slider(xDir, yDir);
+      var zDir = Math.abs(g.translation()[2]) > tolerance ? (g.translation()[2] < 0 ? -1 : 1) : 0;
+      slider(xDir, zDir);
     }
   });
 
