0e40a76b32f48981daea301bcb6467c3c780de7a
numso <dosmun@gmail.com>
2014-10-07 13:28:03 -0600

    added a ton to lifx server

---
+++ lifx-server/server/app.js
@@ -0,0 +1,38 @@
+// --- Require Dependencies ----------------------------------------------------
+
+var   http = require('http');
+var     fs = require('fs');
+var     io = require('socket.io');
+var config = require('config');
+
+var    koa = require('koa');
+var  serve = require('koa-static');
+
+// --- Setup Koa ---------------------------------------------------------------
+
+var app = koa();
+
+app.use(serve('./client'));
+
+// --- Setup Routes ------------------------------------------------------------
+
+fs.readdirSync(__dirname + '/routes').forEach(function (filename) {
+  if (filename[0] === '.') return;
+  require('./routes/' + filename)(app);
+});
+
+var server = http.Server(app.callback());
+
+// --- Setup Sockets -----------------------------------------------------------
+
+io = io(server);
+
+fs.readdirSync(__dirname + '/sockets').forEach(function (filename) {
+  if (filename[0] === '.') return;
+  require('./sockets/' + filename)(io);
+});
+
+// --- Start Listening ---------------------------------------------------------
+
+server.listen(config.port);
+console.log('Server listening on port ' + config.port);
